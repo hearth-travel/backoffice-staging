510f68531d637fc40b42fbeb1f921f9313cbdcd6
Leonardo Rignanese <dev.rignaneseleo@gmail.com>
2022-04-14 13:54:24 +0100

    fix variable google api

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8b01b9fd246fe8a303e589fc8c598af5",
 "favicon.ico": "9e459b35230283f1362af3fe8cfaf128",
-"index.html": "5112b8e43a3180008189b548e9480de0",
-"/": "5112b8e43a3180008189b548e9480de0",
-"main.dart.js": "7ccfa80905e3fc8f181983be2f0b56dd",
+"index.html": "9b2d5482ea5bed0e6d3602a6c1efff09",
+"/": "9b2d5482ea5bed0e6d3602a6c1efff09",
+"main.dart.js": "b3eb9e9b4cc3feeddf82c3351026e5c9",
 "404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "img/hearth.svg": "f4a1f2f7d949b073afbe802e7c1631f3",
 "img/logo.svg": "ec9d1b83f2283db1aef7ac0358432780",
